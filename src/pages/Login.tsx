775dfd3c6c9945184f5134ef093e3964b81348f2
TotiKresna <totikresna123@gmail.com>
2024-10-11 11:08:48 +0700

    edit : form login color

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -70,7 +70,7 @@ export default function Login() {
         borderRadius={4}
         textAlign="center"
         boxShadow="lg"
-        bg={useColorModeValue('white', 'grey.600')}
+        bg={useColorModeValue('white', 'gray.600')}
       >
         <Avatar mt="4" bg="teal.500" />
         <Heading color="teal.400">Welcome</Heading>
